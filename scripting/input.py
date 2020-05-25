218e5a720b58d48ea2a3f4f42a1cb24887341c05
Je suis un dev <admin@jesuisundev.com>
2020-05-25 09:36:19 -0400

    functions on scripting

--- scripting/input.py
+++ scripting/input.py
@@ -1,5 +1,18 @@
 import json
 
-with open('input.json', 'r') as input:
-  obj = json.load(input)
-  print('Hello, ' + obj['name'])
+def openAndReadInput():
+  with open('input.json', 'r') as input:
+    obj = json.load(input)
+    print('Hello, ' + obj['name'])
+
+def openAndCopyInputToOuput():
+  with open('input.json', 'r') as input:
+    obj = json.load(input)
+    
+    with open('output.txt', 'w') as output:
+      output.write(obj['name'] + ' : Hoobies\n')
+      for hobby in obj['hobbies']:
+        output.write(hobby + '\n')
+
+openAndReadInput()
+openAndCopyInputToOuput()
