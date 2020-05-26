b1cff53ea64172fc3afdb094f4a9567c5b237aaa
Je suis un dev <admin@jesuisundev.com>
2020-05-26 09:02:31 -0400

    operators

---
+++ firststep/operator/operator.py
@@ -0,0 +1,29 @@
+superToto = False
+cumul = 0
+
+#if elif else
+if bool(superToto):
+    print('superToto is true')
+elif bool(superToto) is False:
+    print('superToto is not !')
+else:
+    print('dafuk')
+
+# while
+while cumul is not 5:
+  cumul += 1
+  print(cumul)
+
+# for
+for x in range(5):
+  print(x)
+
+while True:
+  print("Type the best number to continue : ")
+  responseFromUser = input()
+
+  if(responseFromUser % 8 == 0):
+      print("YES ! You are free.")
+      break
+  else:
+      print("This is not the best number at all")
