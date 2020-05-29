cf20ae67047a98c528500833ae170161783a1cf1
Je suis un dev <admin@jesuisundev.com>
2020-05-29 09:43:50 -0400

    types

--- basis/types/test.py
+++ basis/types/test.py
@@ -6,3 +6,25 @@ if (x == y):
 
 if (x is not y):
   print('Variable are not equal by identity')
+
+
+def flag (message, decor = '-'):
+    line = decor * len(message)
+
+    print(line)
+    print(message)
+    print(line)
+
+flag('France')
+
+toto, tata = ('supertoto', 'supertata')
+
+print(toto, tata)
+
+testString = "jesuisundev".partition('suis')
+
+print(testString)
+
+
+formatTest = "{0} suis un {1}".format('je', 'dev')
+print(formatTest)
